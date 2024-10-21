82c4cd611a2dfd2f6598d14ae210547d3873d5b2
alvyn2 <87878122+alvyn2@users.noreply.github.com>
2024-10-21 14:14:57 +0000

    message comit 2

--- workspace/HelloWorldSwing.java
+++ workspace/HelloWorldSwing.java
@@ -1,5 +1,7 @@
 import javax.swing.*;        
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class HelloWorldSwing {
     /**
      * Create the GUI and show it.  For thread safety,
@@ -16,7 +18,37 @@ public class HelloWorldSwing {
         JLabel label = new JLabel("Hello World");
         frame.getContentPane().add(label);
 
-        //Display the window.
+        JButton b1 = new JButton("Press me");
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("You Did It");
+                
+            }
+              });   
+
+        frame.getContentPane().add(b1);
+  
+        JButton b2 = new JButton("new phrase");
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("heres another phrase");
+                
+            }
+              });   
+
+        JButton b3 = new JButton("new phrase");
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("heres another phrase");
+                
+            }
+              });   
+        frame.getContentPane().add(b1);
+
+        //Display the window
         frame.pack();
         frame.setVisible(true);
     }
